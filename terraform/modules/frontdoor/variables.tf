c788bcaf47e378ac05e435af3cdcb6c8928dd4b4
Nick Dalalelis <ndalalelis@gmail.com>
2023-01-27 13:05:16 -0500

    Initial commit for front door

---
+++ terraform/modules/frontdoor/variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group" {
+  type        = string
+  description = "The resource group"
+}
+
+variable "application_name" {
+  type        = string
+  description = "The name of your application"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (dev, test, prod...)"
+  default     = "dev"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where all resources in this example should be created"
+}
+
+variable "front_door_sku_name" {
+  type    = string
+  default = "Standard_AzureFrontDoor"
+  validation {
+    condition     = contains(["Standard_AzureFrontDoor", "Premium_AzureFrontDoor"], var.front_door_sku_name)
+    error_message = "The SKU value must be Standard_AzureFrontDoor or Premium_AzureFrontDoor."
+  }
+}
+
+variable "host_name" {
+  type        = string
+  description = "The IPv4 address, IPv6 address or Domain name of the Origin."
+}
