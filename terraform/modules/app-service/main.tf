9d74994ae54cb73cfdac7a63255a1e070f73a981
Ken Schlobohm <keschlob@microsoft.com>
2023-02-16 18:10:48 +0000

    configure scaling #46

--- terraform/modules/app-service/main.tf
+++ terraform/modules/app-service/main.tf
@@ -267,3 +267,55 @@ resource "azurerm_monitor_diagnostic_setting" "app_service_diagnostic" {
     }
   }
 }
+
+# Configure scaling
+resource "azurerm_monitor_autoscale_setting" "airsonicscaling" {
+  name                = "airsonicscaling"
+  resource_group_name = var.resource_group
+  location            = var.location
+  target_resource_id  = azurerm_service_plan.application.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name         = "CpuPercentage"
+        metric_resource_id  = azurerm_service_plan.application.id
+        time_grain          = "PT1M"
+        statistic           = "Average"
+        time_window         = "PT5M"
+        time_aggregation    = "Average"
+        operator            = "GreaterThan"
+        threshold           = 85
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name         = "CpuPercentage"
+        metric_resource_id  = azurerm_service_plan.application.id
+        time_grain          = "PT1M"
+        statistic           = "Average"
+        time_window         = "PT5M"
+        time_aggregation    = "Average"
+        operator            = "LessThan"
+        threshold           = 65
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
