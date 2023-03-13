9fa33da7c0d3d50a6806a2eb10115d45dfa09946
Nikolaos Dalalelis <4593597+nickdala@users.noreply.github.com>
2023-03-13 11:09:41 -0400

    Update setup-initial-env.sh

--- scripts/setup-initial-env.sh
+++ scripts/setup-initial-env.sh
@@ -2,6 +2,8 @@
 
 export PROJECT_ROOT=${PWD}
 export SUBSCRIPTION=
+
+# The APP_NAME is used as the base for naming Azure resources. There is a limit to keep APP_NAME to less than 18 characters.
 export APP_NAME=
 
 # APP_ENVIRONMENT can either be prod or dev
