15ce64f3b15f607fcbb90dfcd7f1f3c32e0571e3
Nick Dalalelis <ndalalelis@gmail.com>
2023-04-07 08:27:01 -0400

    document the setup-local-build-env.sh script

--- scripts/setup-initial-env.sh
+++ scripts/setup-initial-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export PROJECT_ROOT=${PWD}
-export SUBSCRIPTION=
+# The ID of your Azure Subscription, https://learn.microsoft.com/en-us/azure/azure-portal/get-subscription-tenant-id
+export SUBSCRIPTION_ID=
 
 # The APP_NAME is used as the base for naming Azure resources. There is a limit to keep APP_NAME to less than 18 characters.
 export APP_NAME=
@@ -12,4 +12,5 @@ export APP_ENVIRONMENT=dev
 # Telemetry collection is on by default. To opt out, set ENABLE_TELEMETRY to false.
 export ENABLE_TELEMETRY=true
 
+export PROJECT_ROOT=${PWD}
 export TRAININGS_DIR="$PROJECT_ROOT/videos"
--- scripts/setup-local-build-env.sh
+++ scripts/setup-local-build-env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script updates the azure-webapp-maven-plugin section in airsonic-main/pom.xml.
+# The Maven plugin for Azure App Service is used to deploy the app to Azure,
+
 echo "Updating $PROJECT_ROOT/src/airsonic-advanced/airsonic-main/pom.xml"
 
 SED_COMMAND="sed -i"
@@ -8,15 +11,15 @@ if [[ $OSTYPE == 'darwin'* ]]; then
   SED_COMMAND="$SED_COMMAND '' -e"
 fi
 
-export application_name=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_name  | tr -d \")
+application_name=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_name  | tr -d \")
 echo "application_name = $application_name"
 $SED_COMMAND "s/<\!--application_name-->/$application_name/" $PROJECT_ROOT/src/airsonic-advanced/airsonic-main/pom.xml
 
-export application_resource_group=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_resource_group  | tr -d \")
+application_resource_group=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_resource_group  | tr -d \")
 echo "application_resource_group = $application_resource_group"
 $SED_COMMAND "s/<\!--application_resource_group-->/$application_resource_group/" $PROJECT_ROOT/src/airsonic-advanced/airsonic-main/pom.xml
 
-export application_region=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_region  | tr -d \")
+application_region=$(terraform -chdir=$PROJECT_ROOT/terraform show -json | jq .values.outputs.app_service_module_outputs.value.application_region  | tr -d \")
 echo "application_region = $application_region"
 $SED_COMMAND "s/<\!--application_region-->/$application_region/" $PROJECT_ROOT/src/airsonic-advanced/airsonic-main/pom.xml
 
