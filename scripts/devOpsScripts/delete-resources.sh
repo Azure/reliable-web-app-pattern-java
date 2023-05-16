faed3b2c3b19a042be28b7e005eb0d2cc987dacb
Ken Schlobohm <keschlob@microsoft.com>
2023-05-16 19:04:15 +0000

    add teardown script

---
+++ scripts/devOpsScripts/delete-resources.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Usage: ./delete-resources.sh -g <resource-group-name>
+# Example: ./delete-resources.sh -g my-resource-group
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --resource-group|-g)
+      resourceGroupName="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+# check if resource group exists
+if [[ $(az group exists --name "$resourceGroupName") == false ]]; then
+  echo "Resource group $resourceGroupName does not exist."
+  exit 1
+fi
+
+# find key vault from resource group (assumes one vault per resource group)
+keyVaultName=$(az keyvault list --resource-group "$resourceGroupName" --query '[0].name' -o tsv)
+
+# Delete the resource group
+az group delete --name "$resourceGroupName" --yes --no-wait
+
+# Purge the deleted key vault
+az keyvault purge --name "$keyVaultName" --location "your-key-vault-location"
