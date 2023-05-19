0b5c410076fbd329802ce03ce4cce8510757130e
Ken Schlobohm <keschlob@microsoft.com>
2023-05-19 19:30:23 +0000

    making test run more idempotent

--- scripts/devOpsScripts/delete-resources.sh
+++ scripts/devOpsScripts/delete-resources.sh
@@ -33,59 +33,42 @@ fi
 location=$(az group show --name "$resourceGroupName" --query location -o tsv)
 echo "Found $resourceGroupName in $location"
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+resource_types=("Microsoft.Web/Sites" "Microsoft.DBforPostgreSQL/flexibleServers" "Microsoft.Cache/redis" "Microsoft.KeyVault/vaults")
+
+for type in "${resource_types[@]}"; do
+  echo -e "\nChecking if resource exists for ${GREEN}$type${RESET} in $resourceGroupName in $location"
+  resource=$(az resource list --resource-group "$resourceGroupName" --resource-type "$type" --query '[].name' -o tsv)
+
+  if [[ ${#resource} -eq 0 ]]; then
+    echo -e "\tNo resource found for ${RED}$type${RESET} in $resourceGroupName in $location"
+    continue
+  fi
+  echo -e "\tFound resource for $type in $resourceGroupName in $location"
+
+  echo -e "\tChecking for diagnostic settings for $type in $resourceGroupName in $location"
+  diagnosticSettings=$(az monitor diagnostic-settings list --resource-group "$resourceGroupName" --resource-type "$type" --resource $resource --query '[].name' -o tsv)
+  if [[ ${#diagnosticSettings} -gt 0 ]]; then
+    echo -e "\tDeleting diagnostic settings for $type in $resourceGroupName in $location"
+    az monitor diagnostic-settings delete --resource-group "$resourceGroupName" --resource-type "$type" --name "$diagnosticSettings"
+  else
+    echo -e "\tCould not find diagnostic-settings for ${YELLOW}$type${RESET} diagnostics"
+  fi
+done
+
+echo ""
+
 # find key vault from resource group (assumes one vault per resource group)
 keyVaultName=$(az keyvault list --resource-group "$resourceGroupName" --query '[0].name' -o tsv)
 echo "Found $keyVaultName key vault in $resourceGroupName"
 
-# find azure app service name
-appServiceName=$(az webapp list --resource-group "$resourceGroupName" --query '[0].name' -o tsv)
-
-if [[ ${#appServiceName} -gt 0 ]]; then
-  echo "Found $appServiceName app service in $resourceGroupName"
-else 
-  echo "Could not find App Service web app in rg: $resourceGroupName"
-  exit 2
-fi
-
-# find postgres flexible server name
-postgresServerName=$(az postgres flexible-server list --resource-group "$resourceGroupName" --query '[0].name' -o tsv)
-echo "Found $postgresServerName postgres server in $resourceGroupName"
-
-if [[ ${#postgresServerName} -gt 0 ]]; then
-  "Found $postgresServerName postgres server in $resourceGroupName"
-else 
-  echo "Could not find Postgres service in rg: $resourceGroupName"
-  exit 3
-fi
-
-# find azure monitor diagnostic name
-appServiceDiagnosticName=$(az monitor diagnostic-settings list --resource-group "$resourceGroupName" --resource "$appServiceName" --resource-type Microsoft.Web/Sites --query "[0].name" -o tsv)
-echo "Found $appServiceDiagnosticName diagnostics in $resourceGroupName in $location"
-
-# find postgres server diagnostic name
-postgresSqlDiagnosticName=$(az monitor diagnostic-settings list --resource-group "$resourceGroupName" --resource "$postgresServerName" --resource-type Microsoft.DBforPostgreSQL/flexibleServers --query "[0].name" -o tsv)
-echo "Found $postgresSqlDiagnosticName diagnostics in $resourceGroupName in $location"
-
-if [[ ${#appServiceDiagnosticName} -gt 0 ]]; then
-  echo "Deleting $appServiceDiagnosticName diagnostics in $resourceGroupName in $location"
-  # delete the app service diagnostic so that it does not persist through resource group deletion and conflict with TF plan
-  az monitor diagnostic-settings delete --resource-group "$resourceGroupName" --resource "$appServiceName" --name "$appServiceDiagnosticName" --resource-type Microsoft.Web/Sites
-else
-  echo "Skipping delete for appServiceDiagnosticName diagnostics"
-fi
-
-if [[ ${#postgresSqlDiagnosticName} -gt 0 ]]; then
-  echo "Deleting $postgresSqlDiagnosticName diagnostics in $resourceGroupName in $location"
-  # delete the PostgresSQL server diagnostic so that it does not persist through resource group deletion and conflict with TF plan
-  az monitor diagnostic-settings delete --resource-group "$resourceGroupName" --resource "$postgresServerName" --name "$postgresSqlDiagnosticName" --resource-type Microsoft.DBforPostgreSQL/flexibleServers
-else
-  echo "Skipping delete for postgresSqlDiagnosticName diagnostics"
-fi
-
 echo "Deleting resources in $resourceGroupName in $location"
 # Delete the resource group
-az group delete --name "$resourceGroupName" --yes
+#az group delete --name "$resourceGroupName" --yes
 
 echo "Purging $keyVaultName key vault in $resourceGroupName"
 # Purge the deleted key vault
-az keyvault purge --name "$keyVaultName" --location $location
+#az keyvault purge --name "$keyVaultName" --location $location
