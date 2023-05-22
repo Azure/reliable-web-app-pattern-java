6c236bc1c5bfaf5fff503db0a4103dcfe1fb3861
Ken Schlobohm <keschlob@microsoft.com>
2023-05-22 23:29:53 +0000

    add debug plan name

--- terraform/modules/app-service/main.tf
+++ terraform/modules/app-service/main.tf
@@ -47,7 +47,7 @@ resource "azurecaf_name" "app_service_plan" {
 
 # This creates the plan that the service use
 resource "azurerm_service_plan" "application" {
-  name                         = azurecaf_name.app_service_plan.result
+  name                         = "ken-app-svc-20230522-${var.principal_type}"#azurecaf_name.app_service_plan.result
   resource_group_name          = var.resource_group
   location                     = var.location
   
