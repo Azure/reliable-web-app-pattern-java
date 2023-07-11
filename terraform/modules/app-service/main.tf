999cb5e6edc9efaf77a4d7aeadb68b1bb954a761
Nikolaos Dalalelis <4593597+nickdala@users.noreply.github.com>
2023-07-11 19:31:55 -0400

    App registration (#160)
    * initial commit
    
    * updated Terraform Azure provider version
    
    * minor updates
    
    * minor changes
    
    * Single Azure AD App Registration

--- terraform/modules/app-service/main.tf
+++ terraform/modules/app-service/main.tf
@@ -2,16 +2,10 @@ terraform {
   required_providers {
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "1.2.16"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
-      version = "2.33.0"
+      version = "1.2.26"
     }
   }
 }
-
-data "azuread_client_config" "current" {}
 
 resource "azurecaf_name" "app_service_plan" {
   name          = var.application_name
@@ -38,74 +32,6 @@ resource "azurecaf_name" "app_service" {
   name          = var.application_name
   resource_type = "azurerm_app_service"
   suffixes      = [var.location, var.environment]
-}
-
-resource "random_uuid" "admin_role_id" {}
-resource "random_uuid" "user_role_id" {}
-resource "random_uuid" "creator_role_id" {}
-
-resource "azuread_application" "app_registration" {
-  display_name     = "${azurecaf_name.app_service.result}-app"
-  count            = var.principal_type == "User" ? 1 : 0
-  owners           = [data.azuread_client_config.current.object_id]
-  sign_in_audience = "AzureADMyOrg"  # single tenant
-
-  app_role {
-    allowed_member_types = ["User"]
-    description          = "ReadOnly roles have limited query access"
-    display_name         = "ReadOnly"
-    enabled              = true
-    id                   = random_uuid.user_role_id.result
-    value                = "User"
-  }
-
-  app_role {
-    allowed_member_types = ["User"]
-    description          = "Creator roles allows users to create content"
-    display_name         = "Creator"
-    enabled              = true
-    id                   = random_uuid.creator_role_id.result
-    value                = "Creator"
-  }
-
-  required_resource_access {
-    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
-
-    resource_access {
-      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read https://marketplace.visualstudio.com/items?itemName=stephane-eyskens.aadv1appprovisioning
-      type = "Scope"
-    }
-  }
-
-  web {
-    homepage_url  = "https://${var.frontdoor_host_name}"
-    logout_url    = "https://${var.frontdoor_host_name}/logout"
-    redirect_uris = ["https://${var.frontdoor_host_name}/login/oauth2/code/"]
-    implicit_grant {
-      id_token_issuance_enabled     = true
-    }
-  }
-}
-
-resource "azuread_service_principal" "application_service_principal" {
-  application_id               = var.principal_type == "User" ? azuread_application.app_registration[0].application_id : "Ran as ServicePrincipal"
-  count                        = var.principal_type == "User" ? 1 : 0
-  app_role_assignment_required = false
-  owners                       = [data.azuread_client_config.current.object_id]
-}
-
-resource "azuread_application_password" "application_password" {
-  count                 = var.principal_type == "User" ? 1 : 0
-  application_object_id = var.principal_type == "User" ? azuread_application.app_registration[0].object_id : "Ran as ServicePrincipal"
-}
-
-# This is not guidance and is done for demo purposes. The resource below will add the 
-# "Creator" app role assignment for the application of the current user deploying this sample.
-resource "azuread_app_role_assignment" "application_role_current_user" {
-  app_role_id         = azuread_service_principal.application_service_principal[0].app_role_ids["Creator"]
-  count               = var.principal_type == "User" ? 1 : 0
-  principal_object_id = data.azuread_client_config.current.object_id
-  resource_object_id  = azuread_service_principal.application_service_principal[0].object_id
 }
 
 # This creates the linux web app
@@ -187,8 +113,8 @@ resource "azurerm_linux_web_app" "application" {
     SPRING_REDIS_HOST = var.redis_host
     SPRING_REDIS_PORT = var.redis_port
 
-    SPRING_CLOUD_AZURE_ACTIVE_DIRECTORY_CREDENTIAL_CLIENT_ID = var.principal_type == "User" ?  azuread_application.app_registration[0].application_id : var.proseware_client_id
-    SPRING_CLOUD_AZURE_ACTIVE_DIRECTORY_PROFILE_TENANT_ID    = var.principal_type == "User" ?  data.azuread_client_config.current.tenant_id : var.proseware_tenant_id
+    SPRING_CLOUD_AZURE_ACTIVE_DIRECTORY_CREDENTIAL_CLIENT_ID = var.proseware_client_id
+    SPRING_CLOUD_AZURE_ACTIVE_DIRECTORY_PROFILE_TENANT_ID    = var.proseware_tenant_id
 
     SPRING_CLOUD_AZURE_KEYVAULT_SECRET_PROPERTY_SOURCES_0_ENDPOINT=var.key_vault_uri
 
