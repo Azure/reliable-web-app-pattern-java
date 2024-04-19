c11579ec100e17abdf65c941b8ee617534df2bee
Nick Dalalelis <ndalalelis@gmail.com>
2024-04-19 10:55:11 -0400

    Storage account RBAC - precent shared access keys

--- scripts/set-tfstate-vars.sh
+++ scripts/set-tfstate-vars.sh
@@ -9,7 +9,7 @@ environment_name=$(azd env get-values --output json | jq -r .AZURE_ENV_NAME)
 primary_region=$(azd env get-values --output json | jq -r .AZURE_LOCATION)
 existing_container_name=$(azd env get-values --output json | jq -r .RS_CONTAINER_NAME)
 app_environment=$(azd env get-values --output json | jq -r .APP_ENVIRONMENT)
-
+subscription_id=$(azd env get-values --output json | jq -r .AZURE_SUBSCRIPTION_ID)
 
 if [[ $app_environment == "null" ]]; then
     # default to dev
@@ -21,6 +21,21 @@ if [[ $existing_container_name != 'null' ]]; then
     exit 0
 fi
 
+if [[ $subscription_id == 'null' ]]; then
+    echo 'Subscription ID is not set. Please set the subscription ID using azd env set AZURE_SUBSCRIPTION_ID <subscription_id> before running this script.'
+    exit 1
+fi
+
+if [[ $environment_name == 'null' ]]; then
+    echo 'Environment name is not set. Please set the environment name using azd env new <environment_name> before running this script.'
+    exit 1
+fi
+
+if [[ $primary_region == 'null' ]]; then
+    echo 'Primary region is not set. Please set the primary region using azd env set AZURE_LOCATION <region> before running this script.'
+    exit 1
+fi
+
 random_string=$RANDOM
 
 # values must be set by GitHub pipeline as vars that are consistent across workflows because we have one that deploys and another that does teardown
@@ -29,11 +44,13 @@ rs_container_name=terraform
 rs_resource_group=rg-${environment_name}-terraform
 
 az group create --name $rs_resource_group --location $primary_region --tags app-pattern-name=java-rwa environment=$app_environment
-az storage account create --name $rs_storage_account --resource-group $rs_resource_group --location $primary_region --allow-blob-public-access false
+az storage account create --name $rs_storage_account --resource-group $rs_resource_group --location $primary_region --allow-blob-public-access false --allow-shared-key-access false
 
-connection_string=$(az storage account show-connection-string --resource-group $rs_resource_group --name $rs_storage_account --query "connectionString" -o tsv)
+# Assign the current user as a Storage Blob Data Contributor to the storage account
+userId=$(az ad signed-in-user show --query id --output tsv)
+az role assignment create --role "Storage Blob Data Contributor" --assignee-object-id $userId --assignee-principal-type "User" --scope "/subscriptions/$subscription_id/resourceGroups/$rs_resource_group/providers/Microsoft.Storage/storageAccounts/$rs_storage_account"
 
-az storage container create --account-name $rs_storage_account --name $rs_container_name --connection-string $connection_string
+az storage container create --account-name $rs_storage_account --name $rs_container_name --auth-mode login
 
 azd env set RS_STORAGE_ACCOUNT $rs_storage_account
 azd env set RS_CONTAINER_NAME $rs_container_name
